c343fdfab2af949604dbba0b8b0c3f6a05a22ceb
wlzhou <zhou.l.william@gmail.com>
2015-04-11 16:13:53 -0400

    Starting code

---
+++ Meander_IOS/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Meander_IOS
+//
+//  Created by Will on 4/11/15.
+//  Copyright (c) 2015 meander_ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var items: [String] = []
+    @IBOutlet var locationLabel: UILabel?
+    @IBOutlet var activityLabel: UITextField!
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func activityAction(sender: UITextField) {
+        var newActivity = activityLabel!.text
+        self.items.append(newActivity)
+        tableView.reloadData()
+        locationLabel?.text = "Hello"
+    }
+    
+    @IBAction func locationAction(sender: UITextField) {
+        locationLabel?.text = "Hello \(sender.text)"
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+}
+
